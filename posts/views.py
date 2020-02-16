571483e1af1b7d18c5f68976568c494bac130b87
Rafael Faria <rafaelsafaria@gmail.com>
2020-02-16 18:52:14 -0300

    login, logout routes

--- posts/views.py
+++ posts/views.py
@@ -2,22 +2,23 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 
-class HomeView(ListView):
+class HomeView(LoginRequiredMixin, ListView):
 	model = Post
 	template_name = 'posts/index.html'
 	context_object_name = 'blog_posts'
 	ordering = ['-pub_date']
 	paginate_by = 3
 
-class PostView(DetailView):
+class PostView(LoginRequiredMixin, DetailView):
 	model = Post
 	template_name = 'posts/detail.html'
 	context_object_name = 'post'
 
-class PostCreate(CreateView):
+class PostCreate(LoginRequiredMixin, CreateView):
 	model = Post
 	template_name = 'posts/create.html'
 	fields = ['title', 'content']
