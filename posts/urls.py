b51252747de127768ada831dd832ae904db3eac2
Rafael Faria <rafaelsafaria@gmail.com>
2020-02-16 17:01:47 -0300

    create posts page

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import HomeView, PostView
+from .views import HomeView, PostView, PostCreate
 
 urlpatterns = [
     path('', HomeView.as_view(),name='home'),
-    # path('create/', post_create),
+    path('posts/create/', PostCreate.as_view(success_url='/'), name='post-create'),
     path('posts/<int:pk>/', PostView.as_view(), name='post-detail'),
     # path('edit/<post_id>', post_update),
     # path('delete/<post_id>', post_delete),
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 # Create your views here.
 
 
@@ -9,8 +9,19 @@ class HomeView(ListView):
 	model = Post
 	template_name = 'posts/index.html'
 	context_object_name = 'blog_posts'
+	ordering = ['-pub_date']
+	paginate_by = 3
 
 class PostView(DetailView):
 	model = Post
 	template_name = 'posts/detail.html'
 	context_object_name = 'post'
+
+class PostCreate(CreateView):
+	model = Post
+	template_name = 'posts/create.html'
+	fields = ['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
