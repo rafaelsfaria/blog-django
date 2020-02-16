55eca61750deb0ed0593e2139128bdaa5cd5742c
Rafael Faria <rafaelsafaria@gmail.com>
2020-02-16 13:49:33 -0300

    renamed post author field

--- posts/models.py
+++ posts/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
 	title = models.CharField(max_length=50)
 	content = models.TextField()
 	pub_date = models.DateTimeField(auto_now_add=True)
-	post_author = models.ForeignKey(User, on_delete=models.CASCADE)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.title
