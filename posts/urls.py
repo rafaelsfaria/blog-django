eac4b13838429d10628e643e8583fd6eaddd642f
Rafael Faria <rafaelsafaria@gmail.com>
2020-02-16 15:37:27 -0300

    detail view

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, PostView
 
 urlpatterns = [
-    path('', HomeView.as_view()),
+    path('', HomeView.as_view(),name='home'),
     # path('create/', post_create),
-    # path('<post_id>/', post_detail),
+    path('posts/<int:pk>/', PostView.as_view(), name='post-detail'),
     # path('edit/<post_id>', post_update),
     # path('delete/<post_id>', post_delete),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 # Create your views here.
 
 
@@ -9,3 +9,8 @@ class HomeView(ListView):
 	model = Post
 	template_name = 'posts/index.html'
 	context_object_name = 'blog_posts'
+
+class PostView(DetailView):
+	model = Post
+	template_name = 'posts/detail.html'
+	context_object_name = 'post'
