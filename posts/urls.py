1c60cfc0684c04012f810095718314974e57d1e9
Rafael Faria <rafaelsafaria@gmail.com>
2020-02-16 13:50:06 -0300

    added homeview to posts urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import post_list, post_detail, post_create, post_update, post_delete
+from .views import HomeView
 
 urlpatterns = [
-    path('', post_list),
-    path('create/', post_create),
-    path('<post_id>/', post_detail),
-    path('edit/<post_id>', post_update),
-    path('delete/<post_id>', post_delete),
+    path('', HomeView.as_view()),
+    # path('create/', post_create),
+    # path('<post_id>/', post_detail),
+    # path('edit/<post_id>', post_update),
+    # path('delete/<post_id>', post_delete),
 ]
