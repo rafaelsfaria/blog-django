c1e3fe66116882acba059ac325749cdd09b707b8
Rafael Faria <rafaelsafaria@gmail.com>
2020-02-17 13:54:35 -0300

    added category model

--- posts/models.py
+++ posts/models.py
@@ -3,11 +3,21 @@ from django.contrib.auth.models import User
 from datetime import date
 
 # Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=50, default='Tecnologia')
+
+	class Meta():
+		verbose_name_plural = "categories"
+
+	def __str__(self):
+		return self.name
+
 class Post(models.Model):
 	title = models.CharField(max_length=50)
 	content = models.TextField()
 	pub_date = models.DateTimeField(auto_now_add=True)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.title
