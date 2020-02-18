74747304f1ecb01a82f6292843585e1541c805c3
Rafael Faria <rafaelsafaria@gmail.com>
2020-02-17 21:20:46 -0300

    category detail

--- posts/views.py
+++ posts/views.py
@@ -7,27 +7,51 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class HomeView(LoginRequiredMixin, ListView):
-	model = Post
-	template_name = 'posts/index.html'
-	context_object_name = 'blog_posts'
-	ordering = ['-pub_date']
-	paginate_by = 3
+    model = Post
+    template_name = 'posts/index.html'
+    context_object_name = 'blog_posts'
+    ordering = ['-pub_date']
+    paginate_by = 3
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
 
 class PostView(LoginRequiredMixin, DetailView):
-	model = Post
-	template_name = 'posts/detail.html'
-	context_object_name = 'post'
+    model = Post
+    template_name = 'posts/detail.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
 
 class PostCreate(LoginRequiredMixin, CreateView):
-	model = Post
-	template_name = 'posts/create.html'
-	fields = ['title', 'content']
+    model = Post
+    template_name = 'posts/create.html'
+    fields = ['title', 'content']
 
-	def form_valid(self, form):
-		form.instance.author = self.request.user
-		return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
 
 class CategoryDetail(LoginRequiredMixin, DetailView):
-	model = Category
-	template_name = 'categories/detail.html'
-	context_object_name = 'category'
+    model = Category
+    template_name = 'categories/detail.html'
+    context_object_name = 'category'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['posts'] = Post.objects.filter(category__id=context['category'].id)
+        return context
