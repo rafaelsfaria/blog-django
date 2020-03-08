64fc37c4879ff4e2d4f2521c40eb0a8331359be4
Rafael Faria <rafaelsafaria@gmail.com>
2020-03-08 15:41:16 -0300

    removed django-heroku from settings, psql travis

--- blog/settings.py
+++ blog/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,5 +134,3 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-django_heroku.settings(locals())
